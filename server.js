b8574e4af04d09fc2c30aa58a62eadcb25d1be01
MariMax <mma29121983@gmail.com>
2013-02-07 22:36:44 +0600

    Добавил храниение сессии в монго, но пока не разобрался как это работает

--- server.js
+++ server.js
@@ -2,7 +2,31 @@ var express = require('express')
   , app = express()
   , pages = require(__dirname + '/controllers/pages')
   , ejsLocals = require('ejs-locals');
+
+ fs = require('fs');
+
+var MongoStore = require('connect-mongo')(express),
+    mongo = require('mongoose');
+
+var path = require('path');
+if (fs.existsSync('./configDefault.js')) {
+    var configLocal = require('./configDefault.js');
+
+/*    mail = require('mail').Mail(
+        configLocal.getMailConfig());*/
+    conf = configLocal.getSiteConfig();
+}
+else {
+    console.log('Не удалось загрузить настройки');
+}
   
+
+app.use(express.cookieParser()) 
+app.use(express.session({
+    secret: conf.secret,
+    store: new MongoStore(conf.db),
+    maxAge: new Date(Date.now()+3600000)
+  }));
 // configuration settings 
 app.set('views', __dirname + '/views')
 app.engine('ejs',ejsLocals)
@@ -14,6 +38,12 @@ app.use(function (req, res, next) {
   app.locals.route = req.url
   next()
 })
+
+app.use(function (req, res, next) {
+    if (req.session && req.session.user)
+        app.locals.user = req.session.user;
+    next();
+});
 
 app.get('/', function (req, res) { res.redirect('home') })
 
