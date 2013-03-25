9b3eafab6fe4aa54ef0e63cd8f4d250fafcb3739
SRobertZ <robert.sabiryanov@gmail.com>
2013-03-26 01:17:01 +0600

    Tabs

--- site/public/viewmodels/adminViewModel.js
+++ site/public/viewmodels/adminViewModel.js
@@ -4,16 +4,54 @@ function AdminViewModel(cafeId) {
     self.Categories = ko.observableArray([]);
 
     // Operations
-    $.getJSON("/api/cafes/" + cafeId + "/category", function (allData) {
+    $.ajax(
+    {
+        url: "/api/cafes/" + cafeId + "/category",
+        type: "GET",
+        async: false
+    }).done(function (allData) {
+        var first = true;
         $.each(allData, function (index, value) {
-            $.getJSON("/api/cafe/" + cafeId + "/category/" + value._id + "/dishes", function (allData) {
+            $.ajax({
+                url: "/api/cafe/" + cafeId + "/category/" + value._id + "/dishes",
+                type: "GET",
+                async: false
+            }).done(function (allData) {
                 var mappedDishes = $.map(allData, function (item) { return new Dish(item) });
                 var category = new Category(value);
                 category.Dishes(mappedDishes);
+                category.active(first);
+                if (first)
+                    first = false;
                 self.Categories.push(category);
             });
         });
     });
+
+    //$.getJSON("/api/cafes/" + cafeId + "/category", function (allData) {
+    //    var first = true;
+    //    $.each(allData, function (index, value) {
+    //        $.getJSON("/api/cafe/" + cafeId + "/category/" + value._id + "/dishes", function (allData) {
+    //            var mappedDishes = $.map(allData, function (item) { return new Dish(item) });
+    //            var category = new Category(value);
+    //            category.Dishes(mappedDishes);
+    //            category.active(first);
+    //            if (first)
+    //                first = false;
+    //            self.Categories.push(category);
+    //        });
+    //    });
+    //});
+
+    self.setActiveCategory = function (category) {
+        self.active = true;
+        $.each(self.Categories(), function (index, value) {
+            if (value.id() == category.id())
+                value.active(true);
+            else
+                value.active(false);
+        });
+    };
 }
 
 if (document.getElementById("cafeId") != null) {
--- site/public/viewmodels/menuViewModel.js
+++ site/public/viewmodels/menuViewModel.js
@@ -6,12 +6,12 @@ function MenuViewModel(cafeId) {
     self.CafeWorkTime = ko.observable('Время работы не задано');
     self.Categories = ko.observableArray([]);
     self.OrderedDishes = ko.observableArray([]);
-    self.OrderedDishesPrices = ko.computed(function () { 
-    var total = 0;
-    ko.utils.arrayForEach(self.OrderedDishes(), function (dish) {
-                total += dish.Price();
-            })
-            return total; 
+    self.OrderedDishesPrices = ko.computed(function () {
+        var total = 0;
+        ko.utils.arrayForEach(self.OrderedDishes(), function (dish) {
+            total += dish.Price();
+        })
+        return total;
     });
 
 
@@ -32,16 +32,50 @@ function MenuViewModel(cafeId) {
             self.CafePhone(cafe.ClientPhone);
     });
 
-    $.getJSON("/api/cafes/" + cafeId + "/category", function (allData) {
+    $.ajax(
+    {
+        url: "/api/cafes/" + cafeId + "/category",
+        type: "GET",
+        async: false
+    }).done(function (allData) {
+        var first = true;
         $.each(allData, function (index, value) {
-            $.getJSON("/api/cafe/" + cafeId + "/category/" + value._id + "/dishes", function (allData) {
+            $.ajax({
+                url: "/api/cafe/" + cafeId + "/category/" + value._id + "/dishes",
+                type: "GET",
+                async: false
+            }).done(function (allData) {
                 var mappedDishes = $.map(allData, function (item) { return new Dish(item) });
                 var category = new Category(value);
                 category.Dishes(mappedDishes);
+                category.active(first);
+                if (first)
+                    first = false;
                 self.Categories.push(category);
             });
         });
     });
+
+    //$.getJSON("/api/cafes/" + cafeId + "/category", function (allData) {
+    //    $.each(allData, function (index, value) {
+    //        $.getJSON("/api/cafe/" + cafeId + "/category/" + value._id + "/dishes", function (allData) {
+    //            var mappedDishes = $.map(allData, function (item) { return new Dish(item) });
+    //            var category = new Category(value);
+    //            category.Dishes(mappedDishes);
+    //            self.Categories.push(category);
+    //        });
+    //    });
+    //});
+
+    self.setActiveCategory = function (category) {
+        self.active = true;
+        $.each(self.Categories(), function (index, value) {
+            if (value.id() == category.id())
+                value.active(true);
+            else
+                value.active(false);
+        });
+    };
 }
 
 if (document.getElementById("cafeId") != null) {
