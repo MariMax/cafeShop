9e38dad0b28cd5c6c14a3463f6ce8adfc11d2c24
SRobertZ <robert.sabiryanov@gmail.com>
2013-03-27 12:49:29 +0600

    file upload

--- site/controllers/SiteCafeController.js
+++ site/controllers/SiteCafeController.js
@@ -2,19 +2,27 @@ exports.add_routes = function (app) {
     app.get("/cafe/:cafeId/menu", function (req, res)
     { res.render("Cafes/menu", { cafeId: req.params.cafeId }); });
 
-     app.get("/cafe/:cafeId/admin", function (req, res)
+    app.get("/cafe/:cafeId/admin", function (req, res)
     { res.render("Cafes/admin", { cafeId: req.params.cafeId }); });
 
-    app.get("/cafes/newCafe", function (req, res) { 
-    if (req.session.user){
-    res.render("Cafes/newCafe", {userId:req.session.user}); }
-    else res.redirect('users/login');});
+    app.get("/cafes/newCafe", function (req, res) {
+        if (req.session.user) {
+            res.render("Cafes/newCafe", { userId: req.session.user });
+        }
+        else res.redirect('users/login');
+    });
 
     app.get("/cafes/updateValues/:cafeId", function (req, res) {
         if (req.session.user) {
 
-                    res.render("Cafes/admin", { cafeId: req.params.cafeId });
+            res.render("Cafes/admin", { cafeId: req.params.cafeId });
 
+        } else { res.redirect('users/login'); }
+    });
+
+    app.post("/upload", function (req, res) {
+        if (req.session.user) {
+            console.log(req);
         } else { res.redirect('users/login'); }
     });
 }
