82a9750339c6ffb4c3dcd0d7e8d9f3d48bbd3bb1
SRobertZ <robert.sabiryanov@gmail.com>
2013-03-28 01:46:44 +0600

    server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express')
   , nodemailer = require("nodemailer")
   , upload = require('jquery-file-upload-middleware');
 
- //fs = require('fs');
+//fs = require('fs');
 
 var MongoStore = require('connect-mongo')(express),
     mongo = require('mongoose');
@@ -41,16 +41,16 @@ if (path.existsSync('./configDefault.js')) {
     var configLocal = require('./configDefault.js');
     var mailSettings = configLocal.getMailConfig();
 
-        
-        smtpTransport = nodemailer.createTransport("SMTP", {
-    host: mailSettings.host, // hostname
-    secureConnection: true, // use SSL
-    port: 465, // port for secure SMTP
-    auth: {
-        user: mailSettings.username,
-        pass: mailSettings.password
-    }
-});
+
+    smtpTransport = nodemailer.createTransport("SMTP", {
+        host: mailSettings.host, // hostname
+        secureConnection: true, // use SSL
+        port: 465, // port for secure SMTP
+        auth: {
+            user: mailSettings.username,
+            pass: mailSettings.password
+        }
+    });
     conf = configLocal.getSiteConfig();
     SMSconf = configLocal.getSMSConfig();
 }
@@ -59,16 +59,20 @@ else {
 }
 
 // configure upload middleware
-    upload.configure({
-        uploadDir: __dirname + '/site/public/uploads',
-        uploadUrl: '/uploads',
-        imageVersions: {
-            thumbnail: {
-                width: 80,
-                height: 80
-            }
+upload.configure({
+    tmpDir: __dirname + '/site/tmp',
+    publicDir: __dirname + '/site/public',
+    uploadDir: __dirname + '/site/public/uploads',
+    uploadUrl: '/uploads',
+    safeFileTypes: /\.(gif|jpe?g|png)$/i,
+    imageTypes: /\.(gif|jpe?g|png)$/i,
+    imageVersions: {
+        'thumbnail': {
+            width: 80,
+            height: 80
         }
-    });
+    }
+});
 
 app.use('/upload', upload.fileHandler());
 app.use(express.cookieParser());
@@ -76,18 +80,18 @@ app.use(express.bodyParser());
 app.use(express.session({
     secret: conf.secret,
     store: new MongoStore(conf.db),
-    maxAge: new Date(Date.now()+3600000)
-  }));
+    maxAge: new Date(Date.now() + 3600000)
+}));
 // configuration settings 
 app.set('views', __dirname + '/site/views')
-app.engine('ejs',ejsLocals)
+app.engine('ejs', ejsLocals)
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/site/public'))
 
 // mount routes
 app.use(function (req, res, next) {
-  app.locals.route = req.url
-  next()
+    app.locals.route = req.url
+    next()
 })
 
 app.use(function (req, res, next) {
