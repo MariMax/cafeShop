9007628565df7adcfca5d121f1ab9e8d41348fba
MariMax <mma29121983@gmail.com>
2013-04-03 01:47:12 +0600

    Изменил форму заказа

--- site/public/viewmodels/orderViewModel.js
+++ site/public/viewmodels/orderViewModel.js
@@ -1,35 +1,47 @@
+
+
 function OrderViewModel(orderId) {
 
     var self = this;
     self.OrderId = orderId
     self.OrderedDishes = ko.observableArray([]);
-    
+    self.Total =  ko.computed(function () {
+        var total = 0;
+        ko.utils.arrayForEach(self.OrderedDishes(), function (dish) {
+            
+            total += dish.price;
+        })
 
-
-    // Operations
-    
-    self.deleteDish = function (dish) {
-        self.OrderedDishes.push(dish);
-    };
-
+        return total;
+    });
     self.addDish = function (dish) {
-        self.OrderedDishes.push(dish);
-    };
 
-    self.decDish = function (dish) {
-        self.OrderedDishes.push(dish);
+
+        debugger;
+        self.OrderedDishes[0].count++;
+
+
     };
+    
 
     $.getJSON('/api/order/' + orderId, function (order) {
         ko.utils.arrayForEach(order.Dishes, function (dish) {
-            self.OrderedDishes.push(dish);
-        })
+            $.ajax({
+                url: "/api/dishes/" + dish.dishId,
+                type: "GET",
+                async: false
+            }).done(function (b_dish) {
+                
+                self.OrderedDishes.push({ dish: b_dish, count: dish.count, price:b_dish.Price*dish.count });
+            });
+
+        });
     });
 
 }
 
 if (document.getElementById("orderId") != null) {
     var orderId = document.getElementById("orderId").value;
-    if (document.getElementById("cafe_menu_page") != null)
+    if (document.getElementById("order_page") != null)
         ko.applyBindings(new OrderViewModel(orderId), document.getElementById("order_page"));
 }
