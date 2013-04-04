22bdb2ebc484d847573414dad76482614d913fad
SRobertZ <robert.sabiryanov@gmail.com>
2013-04-04 13:24:10 +0600

    ko.custom binding

--- site/public/viewmodels/dishViewModel.js
+++ site/public/viewmodels/dishViewModel.js
@@ -38,10 +38,24 @@ function Dish(data) {
     };
 
 
-
-    self.uploadImage = function () {
-
-    }
-
+    ko.bindingHandlers.uploadImage = {
+        init: function (element, valueAccessor) {
+            debugger;
+            var value = ko.utils.unwrapObservable(valueAccessor()); // Get the current value of the current property we're bound to
+            $(element).fileupload({ dataType: 'json',
+                done: function (e, data) {
+                    debugger;
+                    //var newId = "#" + e.target.id.replace("newPhotoInput", "newPhoto");
+                    //$(newId).attr("style", "background:#fff url(\"" + data.result[0].url + "\") no-repeat center 25%;");
+                    var newId = "#" + e.target.id.replace("newPhotoInput", "newPhotoImage");
+                    $(newId).attr("src", data.result[0].url);
+                    $("#newPhotoTmpUrl").val(data.result[0].url);
+                }
+            });
+        },
+        update: function (element, valueAccessor, allBindingsAccessor) {
+            // Leave as before
+        }
+    };
 
 }
